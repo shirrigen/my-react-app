f1ceaf90927d999dd1a01ef3de001050471a0963
Reagan <shiligen@ReagandeiMac.local>
2018-11-28 18:24:48 +0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Watch the video</Link>
+    <Link to="/page-2/">第二页</Link>
   </Layout>
 )
 
