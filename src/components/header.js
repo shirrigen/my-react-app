7ebfc03fd7a46a3dc5e048d0a98061f81bd69ba4
Reagan <shiligen@ReagandeiMac.local>
2018-11-29 12:27:40 +0800

    navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+    <Link to="/"><img src={require('../images/o-redface-white.svg')} width="44" /></Link>
+    <Link to="/Home">Home</Link>
+    <Link to="/Product">Products</Link>
+    <Link to="/About">About</Link>
+    <Link to="/Free Try"><button>Free Try</button></Link>
     </div>
   </div>
 )
